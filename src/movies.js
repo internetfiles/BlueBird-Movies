88d0cb526f3d31a802367f6c3a1ee551a4cefa06
Jacksparrowpro <70373238+internetfiles@users.noreply.github.com>
2024-06-21 19:48:35 +0300

    Update movies.js

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,10 @@
 export const getSmashystreamUrl = (tmdbID) =>{
-    return `https://chatflix.org/player/movie/823464?url=https://vidsrc.to/embed/${tmdbID`
+    return `https://chatflix.org/player/movie/823464?url=https://vidsrc.to/embed/${tmdbID}`
 }
 export const getSuperembedUrl = (tmdbID) =>{
-    return `https://vidsrc.xyz/embed/${tmdbID`
+    return `https://vidsrc.xyz/embed/${tmdbID}`
 }
 export const get2embedUrl = (tmdbID) =>{
     return `https://vidsrc.xyz/embed/${tmdbID}`
 }
-
 
